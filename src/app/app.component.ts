92abaea73094ab88996a378a4a4fe3a43ea2e8bd
unknown <ikismail7@gmail.com>
2017-12-13 13:31:03 +0530

    Implemented OWL Carousle for home page carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+declare var $: any;
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "app";
+  ngOnInit() {
+    $(document).ready(function() {
+      $(".owl-carousel").owlCarousel({
+        autoHeight: true,
+        center: true,
+        nav: true,
+        items: 1,
+        margin: 30,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+      });
+    });
+  }
 }
