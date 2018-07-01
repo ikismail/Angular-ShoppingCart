3dabccb58dfc223465dc7994f86620099e9004f0
ikismail <ikismail7@gmail.com>
2018-07-01 17:39:53 +0530

    Updated Products Page in Checkout Module

--- src/app/product/checkout/products/products.component.ts
+++ src/app/product/checkout/products/products.component.ts
@@ -1,4 +1,6 @@
+import { ProductService } from "./../../../shared/services/product.service";
 import { Component, OnInit } from "@angular/core";
+import { Product } from "../../../shared/models/product";
 
 @Component({
   selector: "app-products",
@@ -6,7 +8,19 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./products.component.scss"]
 })
 export class ProductsComponent implements OnInit {
-  constructor() {}
+  checkoutProducts: Product[];
+
+  totalPrice = 0;
+  constructor(private productService: ProductService) {
+    const products = productService.getLocalCartProducts();
+
+    this.checkoutProducts = products;
+
+    products.forEach(product => {
+      console.log("Checkout", product.productPrice);
+      this.totalPrice += product.productPrice;
+    });
+  }
 
   ngOnInit() {}
 }
