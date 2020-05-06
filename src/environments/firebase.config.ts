95fa70443ba6ba377528d0955dab9044e65b273d
ikismail <ikismail7@gmail.com>
2020-05-06 21:04:26 +0530

    Removed Typo * Updated SharedModule

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -4,5 +4,5 @@ export const FireBaseConfig = {
   databaseURL: "<YOUR_DATABASE_URL>",
   projectId: "<YOUR_PROJECT_ID>",
   storageBucket: "<YOUR_STORAGE_BUCKETS>",
-  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>"
+  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
 };
